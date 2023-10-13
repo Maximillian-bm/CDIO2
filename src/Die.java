4d68d39098e795e98b25a99433ea65a8dad2929a
Dokey <asgerjd@gmail.com>
2023-10-13 12:14:19 +0200

    Die class has been finished

--- src/Die.java
+++ src/Die.java
@@ -1,3 +1,19 @@
+import java.util.Random;
+
 public class Die {
+
+    private int sides;
+
+    public Die(int input) {
+        this.sides = input;
+
+    }
+
+    public int rollDie(){
+        Random rand = new Random();
+        int n = rand.nextInt(sides);
+        return n+1;
+
+    }
     
 }
