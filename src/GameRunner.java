93e89d5ebdf2b6dd76052be05c5998582519d131
Tobias Melcher <tobiasmelcher@hotmail.com>
2023-10-16 19:36:09 +0200

    GameRunner class

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,5 +1,45 @@
+import javax.xml.crypto.Data;
+
 public class GameRunner {
+    public String input;
+
     public static void main(String[] args) throws Exception {
+        //Creating player objects
+        Player player1 = new Player();
+        Player player2 = new Player();
+        
+        //Creating DatabaseController
+        DatabaseController dataC = new DatabaseController(); 
+
+        //Importing scanner
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+        String input;
+
+        while(scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            
+        }
+
+
+
+
+        
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
         
     }
 }
