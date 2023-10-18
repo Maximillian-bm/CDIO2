4b19e6a19e78d883059a16f4c78f33bcb4f16cdc
Max <maxwork.bm@gmail.com>
2023-10-18 20:39:49 +0200

    even more text added

--- src/UI.java
+++ src/UI.java
@@ -17,12 +17,33 @@ public class UI {
                             " you have rolled a 5, you lose 20 gold and now have a total of ",
                             " you have rolled a 6, you get 180 gold and now have a total of ",
                             " you have rolled a 7, you get no gold and now have a total of ",
-                            " you have rolled a 8, you lose 70 you gold and now have a total of ",
-                            " you have rolled a 9, you get 60 you gold and now have a total of ",
-                            " you have rolled a 10, you lose 80 you gold and now have a total of ",
-                            " you have rolled a 11, you lose 50 you gold and now have a total of ",
-                            " you have rolled a 12, you get 650 you gold and now have a total of "};
-    String[] danishText = {"Hej"};
+                            " you have rolled an 8, you lose 70 gold and now have a total of ",
+                            " you have rolled a 9, you get 60 gold and now have a total of ",
+                            " you have rolled a 10, you lose 80 gold BUT you get an extra turn and you now have a total of ",
+                            " you have rolled an 11, you lose 50 gold and now have a total of ",
+                            " you have rolled a 12, you get 650 gold and now have a total of ",
+                            " you have over 3000 gold and win"};
+    String[] danishText = {"Dette er et 2-spiller spil, I skal hver især oprette en konto ved at skrive det navn, du ønsker din konto skal have, eller skrive et af følgende navne for at vælge en eksisterende konto.",
+                            "Der er ingen konti gemt i databasen.",
+                            "Du bruger din konto til at gemme, hvor mange guldmønter du har. Du vinder guld ved at spille spillet, og det koster 1000 guld at spille det, men fortvivl ikke, for alle nye konti starter med 1000 guld. Du kan også skifte konto ved at skrive 'skift'. Når begge spillere har valgt en konto, kan I skrive 'start' for at begynde spillet. Spiller et, det er din tur. Skriv et navn.",
+                            "Spiller 1 har valgt en konto, nu er det spiller 2's tur.",
+                            "Spiller 2 har valgt en konto.",
+                            "Navnet er allerede i brug af spiller 2. Skriv et andet navn, din tåbelige person.",
+                            "Navnet er allerede i brug af spiller 1. Skriv et andet navn, din tåbelige person.",
+                            "Begge spillere har nu ikke valgt en konto, så spiller 1 begynder ved at vælge eller oprette en konto ved at skrive et navn.",
+                            "Spilleregler og kontrol.",
+                            " Du har slået en 2'er. Du får 250 guld og har nu i alt ",
+                            " Du har slået en 3'er. Du mister 100 guld og har nu i alt ",
+                            " Du har slået en 4'er. Du får 100 guld og har nu i alt ",
+                            " Du har slået en 5'er. Du mister 20 guld og har nu i alt ",
+                            " Du har slået en 6'er. Du får 180 guld og har nu i alt ",
+                            " Du har slået en 7'er. Du får ingen guld og har nu i alt ",
+                            " Du har slået en 8'er. Du mister 70 guld og har nu i alt ",
+                            " Du har slået en 9'er. Du får 60 guld og har nu i alt ",
+                            " Du har slået en 10'er. Du mister 80 guld, MEN får en ekstra tur, og har nu i alt ",
+                            " Du har slået en 11'er. Du mister 50 guld og har nu i alt ",
+                            " Du har slået en 12'er. Du får 650 guld og har nu i alt ",
+                            " Du har over 3000 guld og vinder."};
     String tiles = "-------------------------------------------------------------\n|  0 |+250|-100|+100| -20|+180|  0 | -70| +60|-80?| -50|+650|\n-------------------------------------------------------------";
 
     public UI(int x) {
